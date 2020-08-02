dc65d08dc04e12a68d4ca4a3441c69d0c99c9752
Sathvik Aithala <sathvik@Sathviks-MacBook-Pro.local>
2020-08-02 09:21:52 -0500

    made bar horizontal again

--- plots.js
+++ plots.js
@@ -79,6 +79,7 @@ function buildCharts(sample){
             x: tenVals.reverse(),
             y: tenLabs.reverse(),
             type: "bar",
+            orientation: "h"
       };
 
       var bardata = [bartrace];
